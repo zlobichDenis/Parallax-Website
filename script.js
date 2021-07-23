bfa50681dbab1b0f1c5ec227342064ab0aa0f21d
denisZlobich <sgsdgk9232@gmail.com>
2021-07-23 16:01:09 +0300

    improve css

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
 
+timeline
+  .to('.rock', 3, { y: -300})
+  .to('.girl', 3, { y: -200}, '-=3')
+  .to('.bg1', 3, { y: 50}, '-=3');
+
+
+let scene = new ScrollMagic.Scene({
+  triggerElemnt: 'section',
+  duration: '100%',
+  triggerHook: 0,
+})
+  .setTween(timeline)
+  .setPin('section')
+  .addTo(controller)

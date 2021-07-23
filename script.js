df91d83377e082a1fea4775e338c821446174df4
denisZlobich <sgsdgk9232@gmail.com>
2021-07-23 16:09:31 +0300

    job is done

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ let timeline = new TimelineMax();
 timeline
   .to('.rock', 3, { y: -300})
   .to('.girl', 3, { y: -200}, '-=3')
-  .to('.bg1', 3, { y: 50}, '-=3');
+  .fromTo('.bg1', 3, { y: -50}, { y: 0, duration: 3 }, '-=3')
+  .to('.content', 3, { top: '0%'}, '-=3')
+  .fromTo('.content-images', 3, { opacity: 0}, { opacity: 1, duration: 3})
+  .fromTo('.text', 3, { opacity: 0}, { opacity: 1, duration: 3})
 
 
 let scene = new ScrollMagic.Scene({
   triggerElemnt: 'section',
-  duration: '100%',
+  duration: '300%',
   triggerHook: 0,
 })
   .setTween(timeline)
